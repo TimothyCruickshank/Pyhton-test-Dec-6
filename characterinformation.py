e7ee9899138aea5d2a3626da1dccff84959d29e9
TimothyCruickshank <111780723+TimothyCruickshank@users.noreply.github.com>
2022-12-06 12:35:59 -0500

    first commit

---
+++ characterinformation.py
@@ -0,0 +1,23 @@
+# character class
+class Character:
+    def __init__(self, name, id_num):
+        self.name = name
+        self.id_num = id_num
+
+class Hero(Character):
+    def __init__(self, level, loot):
+        self.level = level
+        self.loot = loot
+
+    
+
+class Boss(Character):
+    def __init__(self, level, hp, attack_damage, lifespan):
+        self.level = level
+        self.hp = hp
+        self. attack_damage = attack_damage
+        self.lifespan = lifespan
+
+
+hero = Character("Knight", 111), Hero(1, 50.00)
+boss = Character("Ogre", 333), Boss(2, 5500, 99, float(Boss.hp/300))
